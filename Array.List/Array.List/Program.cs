879172655096e4d7c941e573db71b72946c95cba
Erick Meraz <emeraz6747@southwestuniversity.edu>
2021-02-06 21:37:33 -0700

    this is my list/array

---
+++ Array.List/Array.List/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array.List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[6];
+
+            myArray[0] = 1;
+            myArray[1] = 2;
+            myArray[2] = 3;
+            myArray[3] = 4;
+            myArray[4] = 5;
+            myArray[5] = 6;
+
+            foreach (var item in myArray)
+            {
+                Console.WriteLine("Count down:" + item);
+            }
+            {
+                List<int> fives = new List<int>() { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50 };
+
+                for (int i = 0; i < fives.Count; i++)
+                {
+                    Console.WriteLine(fives[i]);
+                }
+
+            }
+        }
+    }
+}
+        
+    
